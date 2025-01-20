fa978472f94ca1a31952feda84e12911668cdeb3
jaden <chaewh@firststory.co.kr>
2025-01-20 10:11:08 +0900

    초도커밋

---
+++ src/test/kotlin/com/musinsa/shop/ShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.musinsa.shop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
